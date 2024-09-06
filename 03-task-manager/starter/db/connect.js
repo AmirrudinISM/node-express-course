470a42672168438ef43371cc0364de5a159f6c0c
Amirrudin Ismail bin Adlan Rahim <amirrudin.adlan@uberfusion.com>
2024-09-06 16:05:57 +0800

    add async connection and env

--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
-const connectionString = 'mongodb+srv://amirrudinadlan:byjJYiXRAOB49HQN@nodeexpressproject.yhjt7.mongodb.net/?retryWrites=true&w=majority&appName=NodeExpressProject';
 
-mongoose
-.connect(connectionString,{
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-})
-.then(() => console.log('connected to DB'))
-.catch((err) => console.log(err));
+const connectDB = (url) => {
+    mongoose
+    .connect(url,{
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    })
+}
+
+module.exports = connectDB;
+
