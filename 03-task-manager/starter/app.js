712a3583489587c75963346031656c0105ba387e
Amirrudin Ismail bin Adlan Rahim <amirrudin.adlan@uberfusion.com>
2025-03-04 16:13:24 +0800

    add async wrapper

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -4,11 +4,8 @@ const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
 //middleware
+app.use(express.static('./public'));
 app.use(express.json());
-
-app.get('/hello', (req, res) => {
-    res.send('Dingus');
-})
 
 app.use('/api/v1/tasks', tasks);
 
